99ef1211e1efc4758b5e754b2f81339af777c2a4
Yihui Xie <xie@yihui.name>
2014-02-17 17:28:51 -0600

    more selectize examples
    pageWithSidebar() is replaced with sidebarLayout()

--- 013-selectize/ui.R
+++ 013-selectize/ui.R
@@ -1,18 +1,23 @@
 library(shiny)
 
 states <- setNames(state.abb, state.name)
-shinyUI(pageWithSidebar(
-  headerPanel('Select2 examples'),
-  sidebarPanel(
-    select2Input('e1', '1. A basic example (zero-configuration)', choices = states),
-    select2Input('e2', '2. Multi-select', choices = states, multiple = TRUE),
-    select2Input('e3', '3. Placeholders', choices = c('', states), options = list(placeholder = 'Choose a state')),
-    select2Input(
-      'e4', '4. Clear selection', choices = c('', states),
-      options = list(placeholder = 'Choose a state', allowClear = TRUE)
+shinyUI(fluidPage(
+  titlePanel('Selectize examples'),
+  sidebarLayout(sidebarPanel(
+    selectInput('e0', '0. An ordinary select input', choices = states),
+    selectizeInput('e1', '1. A basic example (zero-configuration)', choices = states),
+    selectizeInput('e2', '2. Multi-select', choices = states, multiple = TRUE),
+    selectizeInput('e3', '3. Item creation', choices = states, options = list(create = TRUE)),
+    selectizeInput(
+      'e4', '4. Max number of options to show', choices = states,
+      options = list(maxOptions = 5)
+    ),
+    selectizeInput(
+      'e5', '5. Max number of items to select', choices = states, multiple = TRUE,
+      options = list(maxItems = 2)
     )
   ),
   mainPanel(
     verbatimTextOutput('e4out')
   )
-))
+)))
