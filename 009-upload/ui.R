dfac6e551ec19a0966d1bf404ff5c89e92942a46
Yihui Xie <xie@yihui.name>
2014-02-17 21:56:52 -0600

    sync with shiny devel version

--- 009-upload/ui.R
+++ 009-upload/ui.R
@@ -5,19 +5,21 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput('file1', 'Choose CSV File',
-                accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+                accept=c('text/csv', 
+								 'text/comma-separated-values,text/plain', 
+								 '.csv')),
       tags$hr(),
       checkboxInput('header', 'Header', TRUE),
       radioButtons('sep', 'Separator',
                    c(Comma=',',
                      Semicolon=';',
                      Tab='\t'),
-                   'Comma'),
+                   ','),
       radioButtons('quote', 'Quote',
                    c(None='',
                      'Double Quote'='"',
                      'Single Quote'="'"),
-                   'Double Quote')
+                   '"')
     ),
     mainPanel(
       tableOutput('contents')
