2808bd0f650b4c9504d882cfe8d87d9d974b9f2b
Yihui Xie <xie@yihui.name>
2014-02-18 01:41:30 -0600

    add the movie example (loading data from remote source)

--- 013-selectize/server.R
+++ 013-selectize/server.R
@@ -2,6 +2,7 @@ library(shiny)
 
 shinyServer(function(input, output) {
   output$e4out <- renderText({
-    paste('You selected', if (input$e4 == '') 'nothing' else input$e4, 'in the example 4')
+    paste('You selected', if (input$e4 == '') 'nothing' else input$e4, 'in the example 4',
+          if (input$e6 != '') sprintf('and the movie "%s"', input$e6))
   })
 })
--- 013-selectize/ui.R
+++ 013-selectize/ui.R
@@ -15,9 +15,50 @@ shinyUI(fluidPage(
     selectizeInput(
       'e5', '5. Max number of items to select', choices = states, multiple = TRUE,
       options = list(maxItems = 2)
-    )
-  ),
+    )),
   mainPanel(
+    selectizeInput('e6', '6. Select a movie', choices = '', options = list(
+      theme = 'movies',
+      valueField = 'title',
+      labelField = 'title',
+      searchField = 'title',
+      options = list(),
+      create = FALSE,
+      render = I("{
+  option: function(item, escape) {
+    var actors = [];
+    for (var i = 0, n = item.abridged_cast.length; i < n; i++) {
+      actors.push('<span>' + escape(item.abridged_cast[i].name) + '</span>');
+    }
+    return '<div>' + '<img src=\"' + escape(item.posters.thumbnail) + '\" alt=\"\">' +
+           '<span class=\"title\">' +
+           '<span class=\"name\">' + escape(item.title) + '</span>' +
+           '</span>' +
+           '<span class=\"description\">' + escape(item.synopsis || 'No synopsis available at this time.') + '</span>' +
+           '<span class=\"actors\">' + (actors.length ? 'Starring ' + actors.join(', ') : 'Actors unavailable') + '</span>' +
+           '</div>';
+  }
+}"),
+      load = I("function(query, callback) {
+  if (!query.length) return callback();
+  $.ajax({
+    url: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json',
+    type: 'GET',
+    dataType: 'jsonp',
+    data: {
+      q: query,
+      page_limit: 10,
+      apikey: '3qqmdwbuswut94jv4eua3j85'
+    },
+    error: function() {
+      callback();
+    },
+    success: function(res) {
+      callback(res.movies);
+    }
+  });
+}")
+    )),
     verbatimTextOutput('e4out')
   )
 )))
