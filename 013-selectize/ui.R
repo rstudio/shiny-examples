8881157de34cb130c6b15d2e5d2665ee78bc0461
Yihui Xie <xie@yihui.name>
2014-02-25 19:56:33 -0600

    Revert "no selectizeInput() now; use selectInput(..., selectize = TRUE) (default)"
    This reverts commit a344119c420396fd0d36f884fceb3ca8167c0d94 and closes #1.

--- 013-selectize/ui.R
+++ 013-selectize/ui.R
@@ -4,20 +4,20 @@ states <- setNames(state.abb, state.name)
 shinyUI(fluidPage(
   titlePanel('Selectize examples'),
   sidebarLayout(sidebarPanel(
-    selectInput('e0', '0. An ordinary select input', choices = states, selectize = FALSE),
-    selectInput('e1', '1. A basic example (zero-configuration)', choices = states),
-    selectInput('e2', '2. Multi-select', choices = states, multiple = TRUE),
-    selectInput('e3', '3. Item creation', choices = states, options = list(create = TRUE)),
-    selectInput(
+    selectInput('e0', '0. An ordinary select input', choices = states),
+    selectizeInput('e1', '1. A basic example (zero-configuration)', choices = states),
+    selectizeInput('e2', '2. Multi-select', choices = states, multiple = TRUE),
+    selectizeInput('e3', '3. Item creation', choices = states, options = list(create = TRUE)),
+    selectizeInput(
       'e4', '4. Max number of options to show', choices = states,
       options = list(maxOptions = 5)
     ),
-    selectInput(
+    selectizeInput(
       'e5', '5. Max number of items to select', choices = states, multiple = TRUE,
       options = list(maxItems = 2)
     )),
   mainPanel(
-    selectInput('e6', '6. Select a movie', choices = '', options = list(
+    selectizeInput('e6', '6. Select a movie', choices = '', options = list(
       theme = 'movies',
       valueField = 'title',
       labelField = 'title',
