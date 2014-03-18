2d4deabb854b179730192c04c81aa3a650a3a878
Yihui Xie <xie@yihui.name>
2014-03-18 12:01:45 -0500

    tweak the datatables demo

--- 012-datatables/ui.R
+++ 012-datatables/ui.R
@@ -1,25 +1,31 @@
 library(shiny)
 library(ggplot2)  # for the diamonds dataset
 
-shinyUI(pageWithSidebar(
-  headerPanel('Examples of DataTables'),
-  sidebarPanel(
-    checkboxGroupInput('show_vars', 'Columns in diamonds to show:', names(diamonds),
-                       selected = names(diamonds)),
-    helpText('For the diamonds data, we can select variables to show in the table;
-             for the mtcars example, we use bSortClasses = TRUE so that sorted
-             columns are colored since they have special CSS classes attached;
-             for the iris data, we customize the length menu so we can display 5
-             rows per page.')
-  ),
-  mainPanel(
-    tabsetPanel(
-      tabPanel('diamonds',
-               dataTableOutput("mytable1")),
-      tabPanel('mtcars',
-               dataTableOutput("mytable2")),
-      tabPanel('iris',
-               dataTableOutput("mytable3"))
+shinyUI(fluidPage(
+  title = 'Examples of DataTables',
+  sidebarLayout(
+    sidebarPanel(
+      conditionalPanel(
+        'input.dataset === "diamonds"',
+        checkboxGroupInput('show_vars', 'Columns in diamonds to show:',
+                           names(diamonds), selected = names(diamonds))
+      ),
+      conditionalPanel(
+        'input.dataset === "mtcars"',
+        helpText('Click the column header to sort a column.')
+      ),
+      conditionalPanel(
+        'input.dataset === "iris"',
+        helpText('Display 5 records by default.')
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel('diamonds', dataTableOutput('mytable1')),
+        tabPanel('mtcars', dataTableOutput('mytable2')),
+        tabPanel('iris', dataTableOutput('mytable3'))
+      )
     )
   )
 ))
