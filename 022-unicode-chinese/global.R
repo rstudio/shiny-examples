65143eabb1f37cea67d57eb773a9dc1c8bee46af
Yihui Xie <xie@yihui.name>
2014-07-17 15:39:22 -0500

    install the font Wenquanyi Zenhei on ShinyApps.io

--- 022-unicode-chinese/global.R
+++ 022-unicode-chinese/global.R
@@ -4,3 +4,12 @@ names(rock2) <- c("面积", "周长", "形状", "渗透性")
 
 # Cairo包的PNG设备似乎无法显示中文字符，强制使用R自身的png()设备
 options(shiny.usecairo = FALSE)
+
+# 请忽略以下代码，它只是为了解决ShinyApps上没有中文字体的问题
+if (system('locate wqy-zenhei.ttc') != 0 &&
+      !file.exists('~/.fonts/wqy-zenhei.ttc') &&
+      file.exists('wqy-zenhei.ttc')) {
+  dir.create('~/.fonts')
+  file.copy('wqy-zenhei.ttc', '~/.fonts')
+  system2('fc-cache', '-f ~/.fonts')
+}
