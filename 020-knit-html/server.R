0d32406185c7ec3092fde294878a304d818cc000
Yihui Xie <xie@yihui.name>
2014-07-23 17:49:24 -0500

    add an example for Jeff: call knit2html() and embed the results in a shiny app

---
+++ 020-knit-html/server.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  regFormula <- reactive({
+    as.formula(paste('mpg ~', input$x))
+  })
+
+  output$report <- renderUI({
+    src <- normalizePath('report.Rmd')
+
+    # temporarily switch to the temp dir, in case you do not have write
+    # permission to the current working directory
+    owd <- setwd(tempdir())
+    on.exit(setwd(owd))
+    knitr::opts_knit$set(root.dir = owd)
+
+    tagList(
+      HTML(knitr::knit2html(text = readLines(src), fragment.only = TRUE)),
+      # typeset LaTeX math
+      tags$script(HTML('MathJax.Hub.Queue(["Typeset", MathJax.Hub]);')),
+      # syntax highlighting
+      tags$script(HTML("if (hljs) $('#report pre code').each(function(i, e) {
+                          hljs.highlightBlock(e)
+                       });"))
+    )
+  })
+
+})
---
+++ 020-knit-html/ui.R
@@ -0,0 +1,15 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  title = 'Embed an HTML report from R Markdown/knitr',
+  sidebarLayout(
+    sidebarPanel(
+      withMathJax(),  # include the MathJax library
+      selectInput('x', 'Build a regression model of mpg against:',
+                  choices = names(mtcars)[-1])
+    ),
+    mainPanel(
+      uiOutput('report')
+    )
+  )
+))
