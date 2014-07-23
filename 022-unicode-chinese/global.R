c202ffca0f8f2965916013b0eb2ef17564bed3ef
Yihui Xie <xie@yihui.name>
2014-07-23 14:01:45 -0500

    for the sake of portability, download wqy-zenhei if it does not exist, but do not include it in version control

--- 022-unicode-chinese/global.R
+++ 022-unicode-chinese/global.R
@@ -6,9 +6,14 @@ names(rock2) <- c("面积", "周长", "形状", "渗透性")
 options(shiny.usecairo = FALSE)
 
 # 请忽略以下代码，它只是为了解决ShinyApps上没有中文字体的问题
-if (system('locate wqy-zenhei.ttc') != 0 &&
-      !file.exists('~/.fonts/wqy-zenhei.ttc') &&
-      file.exists('wqy-zenhei.ttc')) {
+if (Sys.info()[['sysname']] == 'Linux' &&
+      system('locate wqy-zenhei.ttc') != 0 &&
+      !file.exists('~/.fonts/wqy-zenhei.ttc')) {
+  if (!file.exists('wqy-zenhei.ttc'))
+    shiny:::download(
+      'https://github.com/rstudio/shiny-examples/releases/download/v0.10.1/wqy-zenhei.ttc',
+      'wqy-zenhei.ttc'
+    )
   dir.create('~/.fonts')
   file.copy('wqy-zenhei.ttc', '~/.fonts')
   system2('fc-cache', '-f ~/.fonts')
