3781420d4b67c7d35153eb8dd3bcb36785d5bd2a
Winston Chang <winston@stdout.org>
2015-02-18 00:46:08 -0600

    Update wordcloud to use progress from Shiny

--- 082-word-cloud/ui.R
+++ 082-word-cloud/ui.R
@@ -1,28 +1,25 @@
-library(shiny)
-library(shinyIncubator)
-
-shinyUI(fluidPage(
-  progressInit(),
-
+fluidPage(
   # Application title
-  headerPanel("Word Cloud"),
-  
-  # Sidebar with a slider and selection inputs
-  sidebarPanel(width = 5,
-    selectInput("selection", "Choose a book:", 
-                choices = books),
-    actionButton("update", "Change"),
-    hr(),
-    sliderInput("freq", 
-                "Minimum Frequency:", 
-                min = 1,  max = 50, value = 15),
-    sliderInput("max", 
-                "Maximum Number of Words:", 
-                min = 1,  max = 300,  value = 100)
-  ),
-  
-  # Show Word Cloud
-  mainPanel(
-    plotOutput("plot")
+  titlePanel("Word Cloud"),
+
+  sidebarLayout(
+    # Sidebar with a slider and selection inputs
+    sidebarPanel(
+      selectInput("selection", "Choose a book:",
+                  choices = books),
+      actionButton("update", "Change"),
+      hr(),
+      sliderInput("freq",
+                  "Minimum Frequency:",
+                  min = 1,  max = 50, value = 15),
+      sliderInput("max",
+                  "Maximum Number of Words:",
+                  min = 1,  max = 300,  value = 100)
+    ),
+
+    # Show Word Cloud
+    mainPanel(
+      plotOutput("plot")
+    )
   )
-))
+)
