925a0952b1eefc19953259cadd7d596a964b246f
Winston Chang <winston@stdout.org>
2016-08-18 14:35:05 -0500

    Add modal example

---
+++ 114-modal-dialog/app.R
@@ -0,0 +1,13 @@
+shinyApp(
+  ui = basicPage(
+    actionButton("show", "Show modal dialog")
+  ),
+  server = function(input, output) {
+    observeEvent(input$show, {
+      showModal(modalDialog(
+        title = "Important message",
+        "This is an important message!"
+      ))
+    })
+  }
+)
