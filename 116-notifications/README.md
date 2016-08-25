088a7abdedebdb0ebf0302c3a2dbf04fba57da23
Winston Chang <winston@stdout.org>
2016-08-25 14:30:53 -0500

    Add notifications app

---
+++ 116-notifications/README.md
@@ -0,0 +1,1 @@
+This app demonstrates Shiny's notification system.
---
+++ 116-notifications/app.R
@@ -0,0 +1,38 @@
+shinyApp(
+  ui = fluidPage(
+    textInput("txt", "Content", "Text of message"),
+    radioButtons("duration", "Seconds before fading out",
+      choices = c("2", "5", "10", "Never")
+    ),
+    radioButtons("type", "Type",
+      choices = c("default", "message", "warning", "error"), inline = TRUE
+    ),
+    checkboxInput("close", "Close button?", TRUE),
+    actionButton("show", "Show"),
+    actionButton("remove", "Remove")
+  ),
+  server = function(input, output) {
+    id <- NULL
+
+    observeEvent(input$show, {
+      if (input$duration == "Never")
+        duration <- NA
+      else 
+        duration <- as.numeric(input$duration)
+
+      type <- input$type
+      if (is.null(type)) type <- NULL
+
+      id <<- showNotification(
+        input$txt,
+        duration = duration, 
+        closeButton = input$close,
+        type = type
+      )
+    })
+
+    observeEvent(input$remove, {
+      removeNotification(id)
+    })
+  }
+)
