65963fe6bd8c6675867d3b4d398fe69b78645308
Winston Chang <winston@stdout.org>
2016-08-27 21:40:42 -0500

    Update notifications app

--- 116-notifications/app.R
+++ 116-notifications/app.R
@@ -2,14 +2,16 @@ shinyApp(
   ui = fluidPage(
     textInput("txt", "Content", "Text of message"),
     radioButtons("duration", "Seconds before fading out",
-      choices = c("2", "5", "10", "Never")
+      choices = c("2", "5", "10", "Never"),
+      inline = TRUE
     ),
     radioButtons("type", "Type",
-      choices = c("default", "message", "warning", "error"), inline = TRUE
+      choices = c("default", "message", "warning", "error"),
+      inline = TRUE
     ),
     checkboxInput("close", "Close button?", TRUE),
     actionButton("show", "Show"),
-    actionButton("remove", "Remove")
+    actionButton("remove", "Remove most recent")
   ),
   server = function(input, output) {
     id <- NULL
