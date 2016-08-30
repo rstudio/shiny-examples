fcef5f0680c42d541d99679d3837e3e17e0b7d77
Joe Cheng <joe@rstudio.com>
2016-08-30 15:40:43 -0700

    Move palette customization into renderPlot
    Calling palette() at the top level (with no graphics device) causes
    an Rplots.pdf file to be temporarily created in the app dir.

--- 050-kmeans-example/server.R
+++ 050-kmeans-example/server.R
@@ -1,6 +1,3 @@
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-
 function(input, output, session) {
 
   # Combine the selected variables into a new data frame
@@ -13,6 +10,9 @@ function(input, output, session) {
   })
 
   output$plot1 <- renderPlot({
+    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+      "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+
     par(mar = c(5.1, 4.1, 0, 1))
     plot(selectedData(),
          col = clusters()$cluster,
