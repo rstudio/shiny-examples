e136c050dadce22b92fef9b11ae338fa5da3f45e
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2017-10-09 10:54:14 -0400

    Added comment to set height of leafletOutput to a number if not using custom css, closes #68

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -21,9 +21,10 @@ navbarPage("Superzip", id="nav",
         includeScript("gomap.js")
       ),
 
+      # If not using custom CSS, set height of leafletOutput to a number instead of percent
       leafletOutput("map", width="100%", height="100%"),
 
-      # Shiny versions prior to 0.11 should use class="modal" instead.
+      # Shiny versions prior to 0.11 should use class = "modal" instead.
       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
         draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
         width = 330, height = "auto",
