c64cabd6f85acf680d6bcff37be8443d89668c89
Joe Cheng <joe@rstudio.com>
2018-02-28 16:05:20 -0800

    Add regression test for rstudio/shiny#1964

---
+++ 128-plot-dim-error/app.R
@@ -0,0 +1,20 @@
+# Test for https://github.com/rstudio/shiny/issues/1964
+
+library(shiny)
+
+ui <- fluidPage(
+  actionButton("go", "Go"),
+  plotOutput("plot")
+)
+
+server <- function(input, output, session) {
+  w <- eventReactive(input$go, { 400 })
+  h <- eventReactive(input$go, { 400 })
+  
+  output$plot <- renderPlot({
+    plot(cars)
+  }, width = w, height = h)
+}
+
+shinyApp(ui, server)
+
