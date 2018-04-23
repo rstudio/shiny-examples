be67ca163a4354c386804574aada6a34b6f2e041
Joe Cheng <joe@rstudio.com>
2018-04-23 12:02:00 -0700

    Reword explanation

--- 131-renderplot-args/app.R
+++ 131-renderplot-args/app.R
@@ -3,7 +3,7 @@ library(shiny)
 ui <- withTags(fluidPage(
   h3("Test of additional renderPlot args"),
   ol(
-    li("The background of the plot should be entirely transparent."),
+    li("The background of the plot should be the same color as the page background."),
     li("Try resizing the browser's width, make sure it's transparent even after redraw.")
   ),
   style("body { background-color: #A3E4D7; }"),
