9743669d948862479beb199cbd543ddd2e490bcb
Barret Schloerke <schloerke@gmail.com>
2018-10-01 14:26:10 -0400

    Deploy apps in parallel and run knitr example (#107)
    * run deploy in parallel with 10 workers... mainly waiting time, not processing time
    
    * run_all should not try to run docker and should rmarkdown::run 026
    
    * ignore log files produced
    
    * use the cmd arg

--- runall.sh
+++ runall.sh
@@ -10,8 +10,19 @@ shiny () {
   R --quiet --slave -e "shiny::runApp(\"$1\", port=5151, launch.browser=TRUE)"
 }
 
+run_26 () {
+  echo $i
+  R --quiet --slave -e "rmarkdown::run(\"$1\", shiny_args = list(launch.browser = TRUE))"
+}
+
 for i in $( ls -d */ ); do
   if [ "$i" \> "$1" ]; then
-    shiny $i
+    if [ "$i" != "docker/" ]; then
+      if [ "$i" = "026-shiny-inline/" ]; then
+        run_26 $i
+      else
+        shiny $i
+      fi
+    fi
   fi
 done
