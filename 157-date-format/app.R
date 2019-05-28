76df638defa120528addc30af487f4c178f49455
Carson Sievert <cpsievert1@gmail.com>
2019-05-28 16:14:57 -0500

    incorporate winston's feedback

--- 157-date-format/app.R
+++ 157-date-format/app.R
@@ -1,63 +1,68 @@
 library(shiny)
 
 ui <- fluidPage(
-  tags$script("Shiny.addCustomMessageHandler('test_result', function(message) { alert(message); })"),
   HTML(
     "<h3>Informative warnings for mis-specified date strings</h3>
     (Issue <a href='https://github.com/rstudio/shiny/issues/2402'>#2402</a>;
     PR: <a href='https://github.com/rstudio/shiny/pull/2403'>#2403</a>)"
   ),
-  uiOutput("dates")
+  tabsetPanel(
+    tabPanel("Test result", uiOutput("res")),
+    tabPanel("Warnings", verbatimTextOutput("warnings")),
+    tabPanel("Inputs", uiOutput("inputs"))
+  )
 )
 
 
 server <- function(input, output, session) {
 
-  # number of warnings thrown over the course of one tick
-  nWarnings <- 0
+  # variable for tracking warning message
+  warn_messages <- NULL
 
   # turn any warnings produced by calling a function
   # into a notification and optionally call the function
   # again to return the results (use `return = FALSE` if
   # function produces side effects)
-  doNotification <- function(f, ..., return = TRUE) {
+  catchWarning <- function(f, ..., return = TRUE) {
     tryCatch(f(...), warning = function(w) {
-      nWarnings <<- nWarnings + 1
-      msg <- paste("Warning:", w$message)
-      showNotification(msg, type = "warning", duration = NULL)
+      warn_messages <<- c(warn_messages, w$message)
       if (return) f(...)
     })
   }
 
-  output$dates <- renderUI({
+  output$inputs <- renderUI({
     tagList(
-      doNotification(dateInput, "x1", "Mis-specified `value`", value = "2014-13-1"),
-      doNotification(dateInput, "x2", "Mis-specified `min`", min = ""),
-      doNotification(dateInput, "x3", "Mis-specified `max`", max = "abjs"),
-      doNotification(dateRangeInput, "x4", "Mis-specified `start`", start = "null"),
-      doNotification(dateRangeInput, "x5", "Mis-specified `end`", end = "NA"),
-      doNotification(dateRangeInput, "x6", "Mis-specified `min`", min = "21380-03-10"),
-      doNotification(dateRangeInput, "x7", "Mis-specified `max`", max = 12)
+      catchWarning(dateInput, "x1", "Mis-specified `value`", value = "2014-13-1"),
+      catchWarning(dateInput, "x2", "Mis-specified `min`", min = ""),
+      catchWarning(dateInput, "x3", "Mis-specified `max`", max = "abjs"),
+      catchWarning(dateRangeInput, "x4", "Mis-specified `start`", start = "null"),
+      catchWarning(dateRangeInput, "x5", "Mis-specified `end`", end = "NA"),
+      catchWarning(dateRangeInput, "x6", "Mis-specified `min`", min = "21380-03-10"),
+      catchWarning(dateRangeInput, "x7", "Mis-specified `max`", max = 12)
     )
   })
 
-  observe({
-    doNotification(updateDateInput, session, "x1", max = "2014-01-96", return = FALSE)
-    doNotification(updateDateInput, session, "x2", value = "   ", return = FALSE)
-    doNotification(updateDateInput, session, "x3", min = "{}", return = FALSE)
-    doNotification(updateDateRangeInput, session, "x4", end = "x", return = FALSE)
-    doNotification(updateDateRangeInput, session, "x5", start = "29", return = FALSE)
-    doNotification(updateDateRangeInput, session, "x6", max = "val", return = FALSE)
-    doNotification(updateDateRangeInput, session, "x7", min = "$", return = FALSE)
-  }, priority = 1000)
+  outputOptions(output, "inputs", suspendWhenHidden = FALSE)
 
   observe({
-    if (nWarnings == 14) {
-      session$sendCustomMessage("test_result", "Test has passed! 14 warnings were thrown")
-    } else {
-      session$sendCustomMessage("test_result", paste("Test didn't pass :(. Only", nWarnings, "warnings were thrown"))
-    }
-  }, priority = -1000)
+    catchWarning(updateDateInput, session, "x1", max = "2014-01-96", return = FALSE)
+    catchWarning(updateDateInput, session, "x2", value = "   ", return = FALSE)
+    catchWarning(updateDateInput, session, "x3", min = "{}", return = FALSE)
+    catchWarning(updateDateRangeInput, session, "x4", end = "x", return = FALSE)
+    catchWarning(updateDateRangeInput, session, "x5", start = "29", return = FALSE)
+    catchWarning(updateDateRangeInput, session, "x6", max = "val", return = FALSE)
+    catchWarning(updateDateRangeInput, session, "x7", min = "$", return = FALSE)
+  })
+
+  output$warnings <- renderPrint(warn_messages)
+
+  output$res <- renderUI({
+    n <- length(warn_messages)
+    status <- if (n == 14)
+      tags$b("Test passed, move along!", style = "color: green")
+    else
+      p(tags$b("Fail:", style = "color: red"), "expected 14 warnings, but got", n)
+  })
 
 }
 
