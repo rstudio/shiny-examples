b8ce6ae7f584e0265112db37d69b708ba5fd63b4
Winston Chang <winston@stdout.org>
2019-05-31 14:09:34 -0500

    Add imageOutput examples

--- 162-plot-dragging/app.R
+++ 162-plot-dragging/app.R
@@ -1,19 +1,38 @@
+# Generate a png for renderImage
+image_file <- tempfile(fileext='.png')
+png(image_file, width=250, height=250)
+hist(rnorm(1000))
+dev.off()
+onStop(function() {
+  unlink(image_file)
+})
+
+
 shinyApp(
   ui = fluidPage(
     p("The plot below doesn't have any interactions. It should be draggable."),
     plotOutput("plot_no_interact", height = 250, width = 250),
     
-    p("The plot below allows click interactions. It should not be draggable."),
+    p("The plot below allows click interactions. It should NOT be draggable."),
     plotOutput("plot_click", height = 250, width = 250, click = "click"),
 
-    p("The plot below allows hover interactions. It should not be draggable."),
+    p("The plot below allows hover interactions. It should NOT be draggable."),
     plotOutput("plot_hover", height = 250, width = 250, hover = "hover"),
 
-    p("The plot below allows brush interactions. It should not be draggable (dragging will draw a brush, not drag the image)."),
+    p("The plot below allows brush interactions. It should NOT be draggable (dragging will draw a brush, not drag the image)."),
     plotOutput("plot_brush", height = 250, width = 250, brush = "brush"),
 
-    p("The plot below allows click and brush interactions. It should not be draggable."),
-    plotOutput("plot_click_brush", height = 250, width = 250, click = "click2", brush = "brush2")
+    p("The plot below allows click and brush interactions. It should NOT be draggable."),
+    plotOutput("plot_click_brush", height = 250, width = 250, click = "click2", brush = "brush2"),
+
+    p("The image below doesn't have any interactions. It should be draggable."),
+    plotOutput("image_no_interact", height = 250, width = 250),
+
+    p("The image below allows click interactions. It should NOT be draggable."),
+    plotOutput("image_click", height = 250, width = 250, click = "click"),
+
+    p("The image below allows brush interactions. It should NOT be draggable."),
+    plotOutput("image_brush", height = 250, width = 250, brush = "brush")
 
   ),
   server = function(input, output) {
@@ -32,5 +51,17 @@ shinyApp(
     output$plot_click_brush <- renderPlot({
       plot(cars)
     })
+
+    output$image_no_interact <- renderImage({
+      list(src = image_file)
+    }, deleteFile = FALSE)
+
+    output$image_click <- renderImage({
+      list(src = image_file)
+    }, deleteFile = FALSE)
+
+    output$image_brush <- renderImage({
+      list(src = image_file)
+    }, deleteFile = FALSE)
   }
 )
