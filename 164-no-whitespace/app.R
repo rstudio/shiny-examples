7017960e6666c68ccc0347017b2435c60b41ff5f
trestletech <jeff.allen@trestletechnology.net>
2019-07-26 09:24:05 -0500

    Add JS to automatically test whether or not whitespace is detected.

--- 164-no-whitespace/app.R
+++ 164-no-whitespace/app.R
@@ -2,46 +2,34 @@ library(shiny)
 
 # Define UI for app
 ui <- fluidPage(
-
   # App title ----
-  titlePanel("Hello Shiny!"),
+  titlePanel("Suppress Whitespace!"),
 
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      tags$div("Click to learn more about '", tags$a(href="https://shiny.rstudio.com", "Shiny"), "'."),
-      tags$hr(),
-      tags$div("Click to learn more about '", tags$a(href="https://shiny.rstudio.com", "Shiny", .noWS="outside"), "'."),
-      tags$hr(),
-      helpText("The first link above doesn't specify a `.noWS` argument, so spacing is added around the link which isn't the ideal presentation since we want to enquote it. The second link sets `.noWS=\"outside\"` to squash the whitespace around the link.")
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-
-    )
-  )
+  tags$div(id="first", "Click to learn more about '", tags$a(href="https://shiny.rstudio.com", "Shiny"), "'."),
+  tags$div(id="firstOutcome", class="alert alert-info"),
+  tags$hr(),
+  tags$div(id="second", "Click to learn more about '", tags$a(href="https://shiny.rstudio.com", "Shiny", .noWS="outside"), "'."),
+  tags$div(id="secondOutcome", class="alert alert-info"),
+  tags$hr(),
+  helpText("The first link above doesn't specify a `.noWS` argument, so spacing is added around the link which isn't the ideal presentation since we want to enquote it. The second link sets `.noWS=\"outside\"` to squash the whitespace around the link."),
+  tags$script("
+// Some JavaScript to help automate testing
+function testWhitespace(inputId, outputId){
+  var output = $('#' + outputId);
+  if (/'Shiny'/.test($('#' + inputId).text())){
+    output.text('No whitespace detected ^');
+  } else {
+    output.text('Whitespace detected ^');
+  }
+}
+testWhitespace('first', 'firstOutcome');
+testWhitespace('second', 'secondOutcome');
+")
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
 
-  # Histogram of the Old Faithful Geyser Data ----
-  output$distPlot <- renderPlot({
-
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = 10)
-
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-
-    })
 
 }
 
