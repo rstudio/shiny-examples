6b31e7046a40c2ac81b278f8a0de248bab0117c4
Carson <cpsievert1@gmail.com>
2019-09-04 18:45:11 -0500

    make running the test easier

--- 169-prerender-resources/app/app.R
+++ 169-prerender-resources/app/app.R
@@ -1,8 +1,15 @@
 library(shiny)
 
 ui <- fluidPage(
-  "If you see this app and 'hello' in the JS console, then this test passed",
-  tags$script(src = "js/log-app.js")
+  "Below is a test for behavior described ",
+  a("here", href = "https://github.com/rstudio/rmarkdown/pull/1631"),
+  br(),
+  span(
+    id = "test-message",
+    style = "color: red",
+    "Test failed :("
+  ),
+  tags$script(src = "js/run-test.js")
 )
 
 server <- function(input, output) {}
