27227aabddc9e158838b231b76d325d5d37af760
Winston Chang <winston@stdout.org>
2019-09-27 14:40:59 -0500

    Clarify message

--- 171-path-traversal/app.R
+++ 171-path-traversal/app.R
@@ -1,7 +1,7 @@
 shinyApp(
   fluidPage(
     p(
-      "Click on each of the links below. They should report that the file is not found.",
+      "Click on each of the links below. They should report that the file is not found, or is forbidden.",
       "(For issues ", a("httpuv#235", href = "https://github.com/rstudio/httpuv/pull/235"),
       "and ", a("shiny#2566", href = "https://github.com/rstudio/shiny/pull/2566", .noWS = "after"), ")",
     ),
