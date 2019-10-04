d2485e8bbb10081d1ffc9f1dcc15cafc860add7f
Tyner <Tyner.Samantha@bls.gov>
2019-10-04 17:30:03 -0400

    reconfigure with most recent ACS 5 year estimates (2013-17)

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -1,16 +1,24 @@
 library(dplyr)
 
-allzips <- readRDS("data/superzip.rds")
+# allzips <- readRDS("data/superzip.rds")
+# allzips$latitude <- jitter(allzips$latitude)
+# allzips$longitude <- jitter(allzips$longitude)
+# allzips$college <- allzips$college * 100
+# allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
+# row.names(allzips) <- allzips$zipcode
+
+allzips <- readRDS("data/superzip_2017.rds")
 allzips$latitude <- jitter(allzips$latitude)
 allzips$longitude <- jitter(allzips$longitude)
-allzips$college <- allzips$college * 100
 allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
-row.names(allzips) <- allzips$zipcode
+allzips$centile <- allzips$centile  * 100
+# row.names(allzips) <- allzips$zipcode
+
 
 cleantable <- allzips %>%
   select(
-    City = city.x,
-    State = state.x,
+    City = primary_city,
+    State = state,
     Zipcode = zipcode,
     Rank = rank,
     Score = centile,
@@ -21,3 +29,22 @@ cleantable <- allzips %>%
     Lat = latitude,
     Long = longitude
   )
+
+
+
+
+#
+# cleantable <- allzips %>%
+#   select(
+#     City = city.x,
+#     State = state.x,
+#     Zipcode = zipcode,
+#     Rank = rank,
+#     Score = centile,
+#     Superzip = superzip,
+#     Population = adultpop,
+#     College = college,
+#     Income = income,
+#     Lat = latitude,
+#     Long = longitude
+#   )
