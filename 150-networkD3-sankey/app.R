97e27c7039572e52f6c8635200826b07f6e4eab3
Alan Dipert <alan@dipert.org>
2019-12-20 17:12:30 +0000

    Add context to 150-networkD3-sankey

--- 150-networkD3-sankey/app.R
+++ 150-networkD3-sankey/app.R
@@ -1,6 +1,7 @@
 library(networkD3)
 library(shiny)
 library(shinydashboard)
+library(markdown)
 
 ui <- function(req) {
   dashboardPage(
@@ -20,15 +21,30 @@ ui <- function(req) {
     ),
     dashboardBody(
       fluidRow(
-        tags$ol(
-          tags$li(
-            "Selecting a date in the time period on the left should work correctly"
-          ),
-          tags$li(
-            "Clicking the 'Show Progress' should display a progress bar at the bottom right of the page."
-          ),
-          tags$li(
-            "Clicking the 'Bookmark...' button should show a modal that the bookmark link can be copied from."
+        tags$div(style="padding:1em;",
+          HTML(markdownToHTML(fragment.only = TRUE, text="### Background
+  In versions of Shiny prior to 1.4.0, certain Shiny features didn't work
+  because SVGs introduced by the [networkD3](https://github.com/christophergandrud/networkD3)
+  package interfered with Shiny's JavaScript. In particular, Shiny used
+  the 'body' selector under the assumption there would be only one body
+  element on the page. This wasn't  always true when networkD3's sankey
+  plot was involved, as it introduced its own 'body' tags inside SVG markup.
+
+  This problem caused datepicker, progress, and bookmarking dialogs not
+  to work, so we test them all here.
+
+  More information can be found on this PR: https://github.com/rstudio/shiny/pull/2361")),
+          tags$h3("Instructions"),
+          tags$ol(
+            tags$li(
+              "Selecting a date in the time period on the left should work correctly"
+            ),
+            tags$li(
+              "Clicking the 'Show Progress' should display a progress bar at the bottom right of the page."
+            ),
+            tags$li(
+              "Clicking the 'Bookmark...' button should show a modal that the bookmark link can be copied from."
+            )
           )
         )
       ),
