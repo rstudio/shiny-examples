169223f92e3292f0b85863aec94087a77c601374
Barret Schloerke <barret@rstudio.com>
2021-03-02 10:59:54 -0500

    Do not use custom tiles that do not work anymore

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -18,10 +18,7 @@ function(input, output, session) {
   # Create the map
   output$map <- renderLeaflet({
     leaflet() %>%
-      addTiles(
-        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-      ) %>%
+      addTiles() %>%
       setView(lng = -93.85, lat = 37.45, zoom = 4)
   })
 
