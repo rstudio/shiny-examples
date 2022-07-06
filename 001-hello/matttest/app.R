4bf8b409eac952d2ea94b518339a9736f19e2df4
Matt Koscak <92606171+matt-koscak@users.noreply.github.com>
2022-07-06 12:27:36 -0500

    Update app.R

--- 001-hello/matttest/app.R
+++ 001-hello/matttest/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Old Faithful Geyser Data Visual"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
